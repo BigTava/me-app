e364145cfb752c3a6921e275e7df28adca12420f
Tiago Tavares <43147091+BigTava@users.noreply.github.com>
2022-09-04 11:47:00 +0100

    Delete src/state directory

--- src/state/hooks.ts
+++
@@ -1,7 +0,0 @@
-/*import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { AppDispatch, AppState } from "state";
-
-type AppState = ReturnType<typeof store.getState>
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;*/
-
-export {};
